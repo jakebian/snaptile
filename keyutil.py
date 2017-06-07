ca7f177a6dc424c5095fa9877d6437855ac7caff
jake <jake@keplr.io>
2017-06-07 18:09:15 -0400

    refactor into smaller modules

---
+++ keyutil.py
@@ -0,0 +1,46 @@
+from Xlib.keysymdef import latin1
+from functools import reduce
+from Xlib import X
+
+def get_posmap(keymap, disp):
+    posmap = {}
+    for i, row in enumerate(keymap):
+        for j, key in enumerate(row):
+            posmap[keycode(key, disp)] = (i, j)
+    return posmap
+
+def initkeys(keymap, disp, root):
+    return [
+        initkey(
+            keycode(key, disp),
+            root
+        ) for key in reduce(lambda x, y: x + y, keymap)
+    ]
+
+def initkey(code, root):
+
+    root.grab_key(
+        code,
+        X.ControlMask | X.Mod1Mask,
+        1,
+        X.GrabModeAsync,
+        X.GrabModeAsync
+    )
+
+    root.grab_key(
+        code,
+        X.ControlMask | X.Mod1Mask | X.Mod2Mask,
+        1,
+        X.GrabModeAsync,
+        X.GrabModeAsync
+    )
+
+    return code
+
+def keycode(key, disp):
+    return disp.keysym_to_keycode(
+        getattr(
+            latin1,
+            'XK_{}'.format(key)
+        )
+    )
--- snaptile.py
+++ snaptile.py
@@ -1,81 +1,44 @@
 import signal
 from Xlib import display, X
-from Xlib.keysymdef import latin1
-from functools import reduce
 
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, GObject  # noqa
 from window import position
+from keyutil import get_posmap, initkeys
 
-from Xlib.display import Display
-from Xlib import X
-from Xlib import protocol
+def get_globals():
+    displ = display.Display()
+    rt = displ.screen().root
+    rt.change_attributes(event_mask=X.KeyPressMask)
+    return (displ, rt)
 
+disp, root = get_globals()
 
-display = display.Display()
-root = display.screen().root
-root.change_attributes(event_mask=X.KeyPressMask)
-
+lastkey = 0
+lastpressed = False
 keymap = [
     ['Q', 'W', 'E', 'R'],
     ['A', 'S', 'D', 'F'],
     ['Z', 'X', 'C', 'V']
 ]
 
-lastkey = 0
-lastpressed = False
+posmap = get_posmap(keymap, disp)
 
-def keycode(key):
-    return display.keysym_to_keycode(
-        getattr(
-            latin1,
-            'XK_{}'.format(key)
-        )
-    )
-
-def get_posmap(keymap):
-    posmap = {}
-    for i, row in enumerate(keymap):
-        for j, key in enumerate(row):
-            posmap[keycode(key)] = (i, j)
-    return posmap
-
-
-posmap = get_posmap(keymap)
-
-def initkeys():
-    return map(
-        lambda key: initkey(
-            keycode(key)
-        ),
-        reduce(lambda x, y: x + y, keymap)
-    )
-
-
-def initkey(keycode):
-    root.grab_key(
-        keycode,
-        X.ControlMask | X.Mod1Mask,
-        1,
-        X.GrabModeAsync,
-        X.GrabModeAsync
-    )
-
-    root.grab_key(
-        keycode,
-        X.ControlMask | X.Mod1Mask | X.Mod2Mask,
-        1,
-        X.GrabModeAsync,
-        X.GrabModeAsync
-    )
-    return keycode
+def run():
+    initkeys(keymap, disp, root)
+    for event in range(0, root.display.pending_events()):
+        root.display.next_event()
+    GObject.io_add_watch(root.display, GObject.IO_IN, checkevt)
+    print('Snaptile running. Press CTRL+C to cancel.')
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    Gtk.main()
 
 def checkevt(source, condition, handle=None):
-    """ Check keyboard event has all the right buttons pressed. """
     handle = handle or root.display
     global lastkey
     global lastpressed
+
     for _ in range(0, handle.pending_events()):
         event = handle.next_event()
         if event.type == X.KeyPress:
@@ -98,15 +61,5 @@ def handleevt(startkey, endkey):
         posmap[endkey]
     )
 
-
-print('Snaptile running. Press CTRL+C to cancel.')
-
 if __name__ == '__main__':
-    initkeys()
-    signal.signal(signal.SIGINT, signal.SIG_DFL)
-    for event in range(0, root.display.pending_events()):
-        root.display.next_event()
-    GObject.io_add_watch(root.display, GObject.IO_IN, checkevt)
-    Gtk.main()
-
-
+    run()
